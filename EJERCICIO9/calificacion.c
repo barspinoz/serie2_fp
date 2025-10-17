0bee3122dfcf3bbcb5c3681f74c3888cdf9a2b79
barspinoz <109839725+barspinoz@users.noreply.github.com>
2025-10-17 00:47:30 -0600

    EJERCICIO 9

---
+++ EJERCICIO9/calificacion.c
@@ -0,0 +1,50 @@
+/*
+Hecho por Roman Espinosa Salvador
+Ultima revision hecha el 17 de octubre del 2025
+Descripcion del problema: Encontrar los numeros impares hasta un numero N y multiplicarlos
+Este programa se compilo usando clang 19.1.7 para FreeBSD
+*/
+#include <stdio.h>
+
+int main(){
+    /*entrada*/
+    float calificacion = 0;
+    /*salidas*/
+    int caso = 0;
+
+    printf("Escriba su calificacion:\n");
+    scanf("%f", &calificacion);
+
+    /*verificar el tipo de calificacion*/    
+    if (calificacion < 0 || calificacion > 10) {
+        printf("ERROR. ");
+    }
+
+    /*categorizar*/
+    caso = (int)calificacion;
+
+    /*determinar el estado de aprobacion*/
+    switch (caso) {
+        case 0: case 1: case 2: case 3: case 4:
+            printf("Usted NO APROBO\n");
+            break;
+
+        case 5: case 6:
+            printf("Usted APROBO\n");
+            break;
+
+        case 7: case 8: 
+            printf("Usted aprobo NOTABLEMENTE\n");
+            break;
+
+        case 9: printf("Usted aprobo de forma SOBRESALIENTE\n");
+            break;
+
+        case 10: printf("Usted aprobo de forma EXCELENTE\n");
+            break;
+
+        default: printf("Su calificacion NO ES VALIDA\n");
+    }
+
+    return 0;
+}
